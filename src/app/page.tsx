079ae99fa3743a42eccb06aefcec316b2948fb19
Itou-Hiroaki-qqq <chiteijin315@gmail.com>
2025-11-04 11:39:48 +0900

    ログイン機能非表示の解消作業(3)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
-import LayoutClient from './layout.client'
-import HomePageContent from './home-page-content'
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
-export default function HomePage() {
+import LayoutClient from './layout.client';
+import HomePageContent from './home-page-content';
+
+export default async function HomePage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect('/login');
+  }
+
   return (
     <LayoutClient>
       <HomePageContent />
     </LayoutClient>
-  )
+  );
 }
